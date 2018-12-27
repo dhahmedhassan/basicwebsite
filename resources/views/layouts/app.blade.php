54d2996ca12cd005dae5ffcfa8ed0f94106914e2
Ahmed Hassan <dhahmedhassan@protonmail.com>
2018-12-27 08:20:28 +0200

    change some premitions

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Laravel Basicwebsite</title>
+</head>
+<body>
+    <h1>My Laravel Basicwebsite</h1>
+</body>
+</html>

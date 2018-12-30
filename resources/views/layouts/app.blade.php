50ad0266c47bc7530756cbff91cc24e4c35172fc
Ahmed Hassan <dhahmedhassan@protonmail.com>
2018-12-30 06:15:33 +0200

    install npm packages & create navbar view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Laravel Basicwebsite</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+    @include('inc.navbar')
     @yield('content')
 
     @include('inc.sidebar')

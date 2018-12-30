477777c54af0e6d28dcc54946aff2df8f7641c53
Ahmed Hassan <dhahmedhassan@protonmail.com>
2018-12-30 15:09:34 +0200

    add sidebar

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,6 +1,12 @@
 @section('sidebar')
-<div class="sidebar">
-    <h3>Sidebar</h3>
-    This is a Sidebar 
-</div>
-@show
+    <div class="card">
+        <div class="card-body">
+          <h3 class="card-title">Sidebar</h3>
+          <h6 class="card-subtitle mb-2 text-muted">Sidebar subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="/home" class="card-link">home</a>
+          <a href="/about" class="card-link">about</a>
+          <a href="/contact" class="card-link">contact</a>
+        </div>
+      </div>
+    @show

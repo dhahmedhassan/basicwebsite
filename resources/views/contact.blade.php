5be4cb6765fbdd3856223fb5b8119bfa1f193174
Ahmed Hassan <dhahmedhassan@protonmail.com>
2018-12-31 09:27:21 +0200

    add form inputs

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,4 +4,22 @@
     <h1>
         Contact
     </h1>
+    {!! Form::open(['url' => 'contact/submit']) !!}
+        <div class="form-group">
+            {{Form::label('name', 'Name')}}
+            {{Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Enter Name !'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('email', 'E-Mail Address')}}
+            {{Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'example@email.com'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('message', 'Message')}}
+            {{Form::textarea('message', '', ['class' => 'form-control', 'placeholder' => 'Your Message goe\'s HERE ! '])}}
+        </div>
+        <div class="form-group">
+            {{Form::submit('Send!', ['class' => 'btn btn-primary'])}}
+        </div>
+
+    {!! Form::close() !!}
 @endsection
